83f0f139dc46525ec31a069b81a26637013f6521
sarun4144 <sarun4144@gmail.com>
2022-11-22 14:46:05 +0700

    Port commmit 1

---
+++ src/Pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <div className="main">
+        <h1 className="h1Home">
+          <div className="HomeAni1">
+            <div className="fontupAni1">WELCOME TO MY</div>
+          </div>
+          <div className="roller">
+            <span id="rolltext">
+              PORTFOLIO
+              <br />
+            </span>
+          </div>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
