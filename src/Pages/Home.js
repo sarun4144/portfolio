79ab19602865fa731fa5c4247391079bf9e807c2
sarun4144 <sarun4144@gmail.com>
2022-11-23 15:36:26 +0700

    sarun

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,64 @@
 import React from "react";
 import "./Home.css";
+import { BsChevronDoubleDown } from "react-icons/bs";
+import Profile from "./Profile";
 function Home() {
+
+  function scrolldown(e) {
+    e.preventDefault();
+    document.body.scrollTop = 1100; // For Safari
+    document.documentElement.scrollTop = 1100; // For Chrome, Firefox, IE and Opera
+  }
+
+  // Get the button:
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction()
+    
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("TopBTN").style.display = "block";
+    } else {
+      document.getElementById("TopBTN").style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+ 
+
+
   return (
-    <div>
-      <div className="main">
-        <h1 className="h1Home">
-          <div className="HomeAni1">
-            <div className="fontupAni1">WELCOME TO MY</div>
-          </div>
-          <div className="roller">
-            <span id="rolltext">
-              PORTFOLIO
-              <br />
-            </span>
-          </div>
-        </h1>
+    <>
+      <div className="Homecontainer">
+        <div className="main">
+          <h1 className="h1Home">
+            <div className="HomeAni1">
+              <div className="fontupAni1">WELCOME TO MY</div>
+            </div>
+            <div className="roller">
+              <span id="rolltext">
+                <div className="fontupAni2">PORTFOLIO</div>
+                <br />
+              </span>
+            </div>
+          </h1>
+        </div>
+        <br />
+        <button className="scrolldown" onClick={scrolldown}>
+          <BsChevronDoubleDown />
+        </button>
+        <div >
+          <button style={{}} className="myBtn" onClick={topFunction} id="TopBTN" title="Go to top">Top</button>
+        </div>
       </div>
-    </div>
+      <Profile />
+    </>
   );
 }
 
---
+++ src/Pages/Profile.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import "./Profile.css"
+import Profilepic from "../img/Profilepic.jpg"
+import { AiFillCaretDown, AiFillCaretRight } from "react-icons/ai";
+function Profile() {
+    const [Proclick, setProclick] = useState(false);
+    const [ProEducation, setProEducation] = useState(false);
+
+    function Profilestage() {
+        if (Proclick) {
+            setProclick(false)
+            setProEducation(false)
+        } else {
+            setProclick(true)
+            
+        }
+    }
+    function Education() {
+        if (ProEducation) {
+            setProEducation(false)
+
+        } else {
+            setProEducation(true)
+        }
+    }
+
+
+  window.addEventListener('scroll',(event) => {
+    ProfilescrollFunction()
+});
+
+function ProfilescrollFunction() {
+    if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {
+        document.getElementById("Pic").className= "Profilepic";
+        document.getElementById("picslide").className= "slideUp";
+        document.getElementById("picslide2").className= " slideUp";
+    } else {
+        document.getElementById("Pic").className = "Profilepic";
+        document.getElementById("picslide").className= " ";
+        document.getElementById("picslide2").className= " ";
+    }
+  }
+
+    return (
+        <div className='Profilecontainer'>
+            {Proclick
+                ?
+                <>
+                    <h1 className='Profilehead'>Profile</h1>
+                    <div className="row">
+                        <div className="col left">
+                            <div className="" id="picslide">
+                            <img  className='Profilepic2' id="Pic" src={Profilepic} alt="profilepic" width="380" height="500" />
+                            </div>
+                        </div> 
+                            <div class="col right"> 
+                            <div className=" " id="picslide2">
+                            <div className="Profilecards_wrap" id="Warp">
+                                <div className="Profiletext">
+                                    <div className="Profileheaddetail">
+                                        <span >Name : </span>
+                                        <span className="Profiledetail">Sarun Pungkrasae</span>
+                                        <div style={{ marginTop: 10 }}>
+                                            <span >Nickname : </span>
+                                            <span className="Profiledetail">KAI</span>
+                                        </div>
+                                        <div style={{ marginTop: 10 }}>
+                                            <span >Born in: </span>
+                                            <span className="Profiledetail"style={{ color: "#ff5757" }}>THAILAND</span>
+                                            <span className="Profiledetail"> January 4 2001</span>
+                                        </div>
+                                    </div>
+                                    <div style={{ marginTop: 8 }}>
+                                        {
+                                            ProEducation
+                                                ?
+                                                <>
+                                                    <span className="Profileheaddetail" onClick={Education}>Education<AiFillCaretDown /></span>
+                                                    <div className="Profileeducation">
+                                                        2019-<span style={{ color: "#ff5757" }}>NOW</span>
+                                                        <ul className="Profileultext">
+                                                            <li>
+                                                               <span style={{ color: "#ff5757" }}>KMUNTB</span> 
+                                                              
+                                                            </li>
+                                                            <li>
+                                                            <span >KING MONGKUT'S UNIVERSITY OF TECHNOLOGY NORTH BANGKOK</span> 
+                                                              
+                                                            </li>
+                                                            <li>
+                                                            Academic years <span style={{ color: "#ff5757" }}> 4</span> 
+                                                              
+                                                            </li>
+                                                            <li>
+                                                            <span > College of Industrial Technology</span> 
+                                                              
+                                                            </li>
+                                                            <li>
+                                                            <span > Department of Electronics Engineering Technology</span> 
+                                                              
+                                                            </li>
+                                                            <li>
+                                                            <span style={{ color: "#ff5757" }}> GPA 3.04</span> 
+                                                              
+                                                            </li>
+
+
+                                                        </ul>
+                                                    </div>
+                                                </>
+                                                :
+                                                <>
+                                                    <span className="Profileheaddetail" onClick={Education}>Education<AiFillCaretRight /></span>
+
+                                                </>
+                                        }
+                                    </div>
+                                    <br />
+                                    <button className="Profilebutton1" onClick={Profilestage}>ฺBack</button>
+
+                                </div>
+
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </>
+                :
+                <>
+                    <h1 className='Profilehead'>Profile</h1>
+                    <div className="" id="picslide">
+                    <img className='Profilepic' style={{}} src={Profilepic}  id="Pic" alt="profilepic" width="380" height="500" onClick={Profilestage} />
+                    </div>
+                    <br />
+                </>
+            }
+
+        </div>
+    )
+}
+
+export default Profile
