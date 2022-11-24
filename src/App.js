af974126d2d75f835e67556c0423e644b18e08c3
sarun4144 <sarun4144@gmail.com>
2022-11-24 18:58:15 +0700

    sarun

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Home from './Pages/Home'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <BrowserRouter>
-        <Home/>
-    </BrowserRouter>
+    <div>
+      <Home />
+    </div>
+
   );
 }
 
